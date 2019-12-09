3f6a6af01d24dbacf5c09c12cfe8567cd3cc9454
李少軒 <allen93075@gmail.com>
2019-12-09 13:57:58 +0800

    網路爬蟲

---
+++ webcrawler.py
@@ -0,0 +1,18 @@
+import urllib.request as req
+url = "https://www.ettoday.net/feature/%E8%82%A1%E5%B8%82/3900?tt=%E8%82%A1%E5%B8%82%E7%9B%B8%E9%97%9C%E6%96%B0%E8%81%9E"
+#建立request 物件，發送要求避免被拒絕
+request=req.Request(url, headers={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+})
+
+with req.urlopen(request) as response:
+    data = response.read().decode("utf-8")
+
+import bs4
+root=bs4.BeautifulSoup(data , "html.parser")
+titles = root.find_all("div" , class_= "box_0 clearfix")
+# for title in titles:
+#     if titles.h2 != None:
+#         print(titles.h2.string)
+
+
