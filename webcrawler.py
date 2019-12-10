65961c89e78d95f522126b5e1fb13bc935349681
Allen <allen93075@gmail.com>
2019-12-10 13:01:22 +0800

    網爬0.01

--- webcrawler.py
+++ webcrawler.py
@@ -1,5 +1,5 @@
 import urllib.request as req
-url = "https://www.ettoday.net/feature/%E8%82%A1%E5%B8%82/3900?tt=%E8%82%A1%E5%B8%82%E7%9B%B8%E9%97%9C%E6%96%B0%E8%81%9E"
+url = "https://www.ettoday.net/feature/%E8%82%A1%E7%A5%A8/6744?tt=%E8%82%A1%E7%A5%A8%E7%9B%B8%E9%97%9C%E6%96%B0%E8%81%9E"
 #建立request 物件，發送要求避免被拒絕
 request=req.Request(url, headers={
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
