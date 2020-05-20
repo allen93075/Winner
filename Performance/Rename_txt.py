1c10befa351de8d8c5c43c11787172952204004b
TIENYOU <tienyou1998@gmail.com>
2020-05-20 17:48:59 +0800

    update Rename_txt.py

--- Performance/Rename_txt.py
+++ Performance/Rename_txt.py
@@ -1,21 +1,19 @@
 import os
 
 def renamefile():
-    # make a duplicate of an existing file
+    # 海龜
     try:
         os.rename(r'Performance/turtle.txt',
                   r'Performance/海龜.txt')
         # Do something with the file
     except IOError:
         print("File already renamed")
-
     try:
         os.rename(r'Performance/turtle_FF.txt',
                   r'Performance/海龜+固定分數.txt')
         # Do something with the file
     except IOError:
         print("File already renamed")
-
     try:
         os.rename(r'Performance/turtle_FR.txt',
                   r'Performance/海龜+固定比率.txt')
@@ -40,13 +38,142 @@ def renamefile():
           # Do something with the file
     except IOError:
          print("File already renamed")
+
+    # 區間突破
     try:
         os.rename(r'Performance/RangeBreak.txt',
                   r'Performance/區間突破.txt')
           # Do something with the file
     except IOError:
          print("File already renamed")
+    try:
+        os.rename(r'Performance/RangeBreak_FF.txt',
+                  r'Performance/區間突破+固定分數.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/RangeBreak_FR.txt',
+                  r'Performance/區間突破+固定比率.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/RangeBreak_F.txt',
+                  r'Performance/區間突破+最佳F值.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/RangeBreak_kelly.txt',
+                  r'Performance/區間突破+凱利.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/RangeBreak_william.txt',
+                  r'Performance/區間突破+LarryWilliams.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
 
+    # 移動均線
+    try:
+        os.rename(r'Performance/MA.txt',
+                  r'Performance/移動均線.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/MA_FF.txt',
+                  r'Performance/移動均線+固定分數.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/MA_FR.txt',
+                  r'Performance/移動均線+固定比率.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/MA_kelly.txt',
+                  r'Performance/移動均線+凱利.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/MA_F.txt',
+                  r'Performance/移動均線+最佳F值.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/MA_william.txt',
+                  r'Performance/移動均線+LarryWilliams.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
 
-# if __name__ == "__main__":
-#     renamefile()
+    # BigBar
+    try:
+        os.rename(r'Performance/BigBar_FF.txt',
+                  r'Performance/BigBar+固定分數.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/BigBar_FR.txt',
+                  r'Performance/BigBar+固定比率.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/BigBar_F.txt',
+                  r'Performance/BigBar+最佳F值.txt')
+        # Do something with the file
+    except IOError:
+        print("File already renamed")
+    try:
+        os.rename(r'Performance/BigBar_kelly.txt',
+                  r'Performance/BigBar+凱利.txt')
+        # Do something with the file
+    except IOError:
+        print("File already renamed")
+    try:
+        os.rename(r'Performance/BigBar_william.txt',
+                  r'Performance/BigBar+LarryWilliams.txt')
+        # Do something with the file
+    except IOError:
+        print("File already renamed")
+    # CCI
+    try:
+        os.rename(r'Performance/CCI_FF.txt',
+                  r'Performance/CCI+固定分數.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/CCI_FR.txt',
+                  r'Performance/CCI+固定比率.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/CCI_F.txt',
+                  r'Performance/CCI+最佳F值.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/CCI_kelly.txt',
+                  r'Performance/CCI+凱利.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/CCI_william.txt',
+                  r'Performance/CCI+LarryWilliam.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
