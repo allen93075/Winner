ae1a69e97009989b9486cf6866eec0c945c53db3
TIENYOU <tienyou1998@gmail.com>
2020-05-20 17:34:17 +0800

    update Rename_txt.py

--- Performance/Rename_txt.py
+++ Performance/Rename_txt.py
@@ -5,21 +5,47 @@ def renamefile():
     try:
         os.rename(r'Performance/turtle.txt',
                   r'Performance/海龜.txt')
-        os.rename(r'Performance/turtle_FF.txt',
-                  r'/Performance/海龜+固定分數.txt')
-        os.rename(r'Performance/turtle_FR.txt',
-                  r'/Performance/海龜+固定比率.txt')
-        os.rename(r'Performance/turtle_kelly.txt',
-                  r'/Performance/海龜+凱利.txt')
-        os.rename(r'Performance/turtle_optimalF.txt',
-                  r'/Performance/海龜+最佳F值.txt')
-        os.rename(r'Performance/turtle_william.txt',
-                  r'/Performance/海龜+LarryWilliams.txt')
         # Do something with the file
     except IOError:
         print("File already renamed")
 
+    try:
+        os.rename(r'Performance/turtle_FF.txt',
+                  r'Performance/海龜+固定分數.txt')
+        # Do something with the file
+    except IOError:
+        print("File already renamed")
 
+    try:
+        os.rename(r'Performance/turtle_FR.txt',
+                  r'Performance/海龜+固定比率.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/turtle_kelly.txt',
+                  r'Performance/海龜+凱利.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/turtle_optimalF.txt',
+                  r'Performance/海龜+最佳F值.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/turtle_william.txt',
+                  r'Performance/海龜+LarryWilliams.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
+    try:
+        os.rename(r'Performance/RangeBreak.txt',
+                  r'Performance/區間突破.txt')
+          # Do something with the file
+    except IOError:
+         print("File already renamed")
 
 
 # if __name__ == "__main__":
